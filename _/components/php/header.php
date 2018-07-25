00c6d3ec3e409e980aeacb37960590ae635ab49b
Celeste0606 <celeste0606@fullsail.edu>
2018-07-25 12:01:04 -0400

    added template

---
+++ _/components/php/header.php
@@ -0,0 +1,28 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+				<a href="index.php"><img src="images/misc/logo.png" alt="Logo"></a>
+			</section><!-- branding -->
+
+			<section class="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+							<li><a tabindex="-1" href="artists.php">All </a></li>
+							<li class="divider"></li>
+						  <li><a tabindex="-1" href="#">name</a></li>
+						  <li><a tabindex="-1" href="#">name</a></li>
+						  <li><a tabindex="-1" href="#">name</a></li><!--href e.g. page.php#name -->
+
+						</ul><!-- dropdown menu -->
+					</li>
+					<li><a href="register.php">Register</a></li>
+				</ul><!-- nav -->
+			</section><!-- navbar -->
+
+		</header><!-- header -->
+	</div><!-- column -->
+</div><!-- content -->
